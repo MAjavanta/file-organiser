891d23c16bf139aa18219b10ab173d5226348d22
MAjavanta <ma.javanta@gmail.com>
2026-05-01 00:11:48 +0100

    List out files from a directory

--- FileOrganiser.Core/Program.cs
+++ FileOrganiser.Core/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to File Organiser!");
+string? inputPath;
+Console.WriteLine("Please provide a directory path:");
+while (true)
+{
+    inputPath = Console.ReadLine();
+    if (inputPath is null || inputPath.Length == 0)
+    {
+        Console.WriteLine("Must provide a path");
+        continue;
+    }
+    break;
+}
+if (Directory.Exists(inputPath))
+{
+    Console.WriteLine($"Printing files from {inputPath}");
+    var directoryInfo = new DirectoryInfo(inputPath);
+    var files = directoryInfo.EnumerateFileSystemInfos();
+    foreach (var file in files)
+    {
+        Console.WriteLine(file);
+    }
+}
+else
+{
+    Console.WriteLine("The directory provided does not exist.");
+}
+
